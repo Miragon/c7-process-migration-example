5665069fba0e738fcb88420a614e2fc1870aaebb
Andreas Riepl <andreas.riepl@miragon.io>
2025-08-11 15:29:43 +0200

    init project

---
+++ src/main/java/io/miragon/c7processmigrationexample/C7ProcessMigrationExampleApplication.java
@@ -0,0 +1,13 @@
+package io.miragon.c7processmigrationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C7ProcessMigrationExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C7ProcessMigrationExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/miragon/c7processmigrationexample/C7ProcessMigrationExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.miragon.c7processmigrationexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C7ProcessMigrationExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
